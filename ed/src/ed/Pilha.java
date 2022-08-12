50dbfa42135029e48cedddb73b34c058a52388a9
willengcomp <will.engcomp.2014@gmail.com>
2022-08-12 13:32:18 -0300

    Terceiro commit - Pilha

---
+++ ed/src/ed/Pilha.java
@@ -0,0 +1,28 @@
+package ed;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Pilha {
+	//trabalharemos com uma pilha de Strings
+	
+	private List<String> nomes = new LinkedList<String>();
+	
+	public void insere(String nome) {
+		nomes.add(nome);
+	}
+	
+	public String remove() {
+		return nomes.remove(nomes.size() - 1);
+	}
+	
+	public boolean vazia() {
+		return nomes.isEmpty();
+	}
+	
+	@Override
+	public String toString() {
+		return nomes.toString();
+	}
+	
+}
